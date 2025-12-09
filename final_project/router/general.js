0614eea883f05f91f4580f366e11ef3131b743b3
rpal123 <rstgopal@gmail.com>
2025-12-09 05:09:16 -0500

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,8 +32,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
 
+  const author = req.params.author;
+
+  for (let id in books) {
+    if (books[id].author === author) {
+        res.send(books[id]); 
+    }
+  };
 });
 
 
